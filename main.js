38bb0a6ea06b95a40749dda9790bd198ceaebd71
rahatkabirrisbe <107284570+rahatkabirrisbe@users.noreply.github.com>
2022-06-23 20:03:14 +0600

    tweet input dynamically added

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('he');
+const formEl = document.querySelector('form')
+const inputEl = document.querySelector('.tweet-input')
+const ol = document.querySelector('ol')
+// const searchEl = document.querySelector('')
+let data = [];
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputValue = inputEl.value;
+    console.log(inputValue);
+    createList(inputValue);
+    resetValue(inputEl)
+})
+
+function dataTrack(){
+    // data = {
+    //     id:
+    //     tweet:
+    // }
+}
+function getID(){
+
+}
+function createList(value){
+    const listEl = `<li id="">${value} <i class="fa fa-trash"></i></li>`
+    ol.insertAdjacentHTML('beforeend', listEl)
+
+}
+function resetValue(inputEl){
+    inputEl.value = '';
+}
